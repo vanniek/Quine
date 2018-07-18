1a7cff5b2d1f3356b03e8df9c0c8ba417915bade
Vannie <34667439+vanniek@users.noreply.github.com>
2018-07-18 09:40:33 -0400

    Update Player.cs

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -71,9 +71,6 @@ namespace Completed
 			int horizontal = 0;  	//Used to store the horizontal move direction.
 			int vertical = 0;		//Used to store the vertical move direction.
 			
-			//Check if we are running either in the Unity editor or in a standalone build.
-#if UNITY_STANDALONE || UNITY_WEBPLAYER
-			
 			//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
 			horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
 			
